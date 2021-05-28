131c7d0b383977c660edf1296be73204959d173d
Augusto Rivera <rivera_augusto@protonmail.com>
2021-05-28 18:10:46 -0500

    Initial Commit

---
+++ src/test/java/com/rivwox/racing/RacingApplicationTests.java
@@ -0,0 +1,14 @@
+/*
+package com.rivwox.racing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
